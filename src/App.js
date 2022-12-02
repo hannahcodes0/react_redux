35328e44fe07a8467d58d42b6bdeefb1a7b93fff
hannahcodes0 <hannahhayoungkim2@gmail.com>
2022-12-02 21:38:31 +0900

    dispatch and action

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 // src/App.js
 
 import React from "react";
-import { useSelector } from "react-redux"; // import 해주세요.
+import { useSelector, useDispatch } from "react-redux"; // import 해주세요.
 
 const App = () => {
-  // const counterStore = useSelector((state)=>state);
-  const counterStore = useSelector(function (state) {
-    return state;
-  });
+  const dispatch = useDispatch();
   const number = useSelector((state) => state.counter.number);
-  console.log(counterStore);
-  console.log(number);
-  return <div></div>;
+  // 카운터의 숫자를 확인하는 코드. 변경이 되는지 확인할때
+  console.log(number); // 숫자 출력
+  return (
+    <div>
+      {number}
+      <button
+        onClick={() => {
+          dispatch({ type: "PLUS_ONE" });
+        }}
+      >
+        +1
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "MINUS_ONE" });
+        }}
+      >
+        -1
+      </button>
+    </div>
+  );
 };
 
 export default App;
