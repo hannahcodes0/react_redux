096a116164d865508185212f7bca9764c9d4e143
hannahcodes0 <hannahhayoungkim2@gmail.com>
2022-12-03 23:16:55 +0900

    action creator

--- src/App.js
+++ src/App.js
@@ -1,32 +1,41 @@
-// src/App.js
+// src/modules/counter.js
 
-import React from "react";
-import { useSelector, useDispatch } from "react-redux"; // import 해주세요.
+// 추가된 코드 👇 - 액션 value를 상수들로 만들어 줍니다. 보통 이렇게 한곳에 모여있습니다.
+const PLUS_ONE = "PLUS_ONE";
+const MINUS_ONE = "MINUS_ONE";
 
-const App = () => {
-  const dispatch = useDispatch();
-  const number = useSelector((state) => state.counter.number);
-  // 카운터의 숫자를 확인하는 코드. 변경이 되는지 확인할때
-  console.log(number); // 숫자 출력
-  return (
-    <div>
-      {number}
-      <button
-        onClick={() => {
-          dispatch({ type: "PLUS_ONE" });
-        }}
-      >
-        +1
-      </button>
-      <button
-        onClick={() => {
-          dispatch({ type: "MINUS_ONE" });
-        }}
-      >
-        -1
-      </button>
-    </div>
-  );
+// 추가된 코드 👇 - Action Creator를 만들어 줍니다.
+export const plusOne = () => {
+  return {
+    type: PLUS_ONE,
+  };
 };
 
-export default App;
+export const minusOne = () => {
+  return {
+    type: MINUS_ONE,
+  };
+};
+
+// 초기 상태값
+const initialState = {
+  number: 0,
+};
+
+// 리듀서
+const counter = (state = initialState, action) => {
+  switch (action.type) {
+    case PLUS_ONE: // case에서도 문자열이 아닌, 위에서 선언한 상수를 넣어줍니다.
+      return {
+        number: state.number + 1,
+      };
+    case MINUS_ONE: // case에서도 문자열이 아닌, 위에서 선언한 상수를 넣어줍니다.
+      return {
+        number: state.number - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+export default counter;
